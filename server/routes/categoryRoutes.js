63ecccf8e49481a49ce63d191ada6d872fa66c81
Abhishek-Yadav11 <abhishek1102yadav@gmail.com>
2025-01-05 20:45:16 +0530

    Week 3

--- server/routes/categoryRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getNewsByCategory } = require("../controllers/categoriesController");
-
-const router = express.Router();
-
-// Route: GET /api/categories/:category
-router.get("/:category", getNewsByCategory);
-
-module.exports = router;
--- server/routes/newsRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { getNews } = require("../controllers/newsController");
-
-const router = express.Router();
-
-// Route: GET /api/news
-router.get("/", getNews);
-
-module.exports = router;
